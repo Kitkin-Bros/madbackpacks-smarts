bab786cac5ee11cb1bdc6e44ac052c3630cceafb
Roman <41580710+PunkGagarin@users.noreply.github.com>
2022-09-28 13:12:04 +0300

    MBPU is Burnable

--- Smarts/ERC20/MBPU.sol
+++ Smarts/ERC20/MBPU.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract MBPU is ERC20 {
+contract MBPU is ERC20, ERC20Burnable {
     constructor(uint256 initialSupply) ERC20("MadbackpackUtiliy", "MBPU") {
         _mint(msg.sender, initialSupply);
     }
